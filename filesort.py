22431d622c2edba725e36f2b9a6bd0ef6f79985b
Alan Le <aal.py@yandex.ru>
2024-10-06 21:52:10 +0400

    edit 3 task

--- filesort.py
+++ filesort.py
@@ -1,164 +1,24 @@
-x = 0
-y = 0
-z = 0
+import os
 
-with open('1.txt', encoding='utf-8') as f:
-    lines = f.readlines()
-    x = (len(lines))
-with open('2.txt', encoding='utf-8') as f:
-    lines = f.readlines()
-    y = (len(lines))
-with open('3.txt', encoding='utf-8') as f:
-    lines = f.readlines()
-    z = (len(lines))
+dir = os.getcwd()
+txt_dir = 'Sorting'
+dir_path = os.path.join(dir, txt_dir)
+merge = []
 
-values = [x, y, z]
-max_value = max(values)
-min_value = min(values)
+for filename in os.listdir(dir_path):
+    with open(os.path.join(dir_path, filename), 'r', encoding='utf-8') as f:
+        text = f.readlines()
+        len_ = len(text)
+        merge.append((filename, len_, text)) 
 
-if min_value == x and max_value == y:
-    
-    with open('1.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'w', encoding='utf-8') as output_file:
-        output_file.write("1.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(x) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text + "\n")
-        
-    with open('3.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write("3.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(z) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text + "\n")
+def get_len_text(file_len): 
+    return file_len[1]   
 
-    with open('2.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write("2.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(y) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text)
+merge.sort(key=get_len_text) 
 
-elif min_value == y and max_value == x:
-
-    with open('2.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'w', encoding='utf-8') as output_file:
-        output_file.write("2.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(y) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text + "\n")
-        
-    with open('3.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write("1.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(z) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text + "\n")
-
-    with open('2.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write("3.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(x) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text)
-
-elif min_value == x and max_value == z:
-
-    with open('1.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'w', encoding='utf-8') as output_file:
-        output_file.write("1.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(x) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text + "\n")
-        
-    with open('2.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write("2.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(y) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text + "\n")
-
-    with open('3.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write("3.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(z) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text)
-
-elif min_value == z and max_value == x:
-
-    with open('3.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'w', encoding='utf-8') as output_file:
-        output_file.write("3.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(z) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text + "\n")
-        
-    with open('2.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write("2.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(y) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text + "\n")
-
-    with open('1.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write("1.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(x) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text)
-
-elif min_value == y and max_value == z:
-
-    with open('2.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'w', encoding='utf-8') as output_file:
-        output_file.write("2.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(y) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text + "\n")
-        
-    with open('1.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write("1.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(x) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text + "\n")
-
-    with open('3.txt', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write("3.txt\n") 
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(str(z) + "\n")    
-    with open('mix.txt', 'a', encoding='utf-8') as output_file:
-        output_file.write(text)    
-
-
+with open(os.path.join(dir, 'mix.txt'), 'w', encoding='utf-8') as outfile:
+    for filename, len_, text in merge:
+        outfile.write(str(filename) + "\n")
+        outfile.write(str(len_) + "\n")
+        outfile.writelines(text) 
+        outfile.write("\n")
